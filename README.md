15b720da12b128326a35bc30a7859684c2440561
Marco Ceppi <marco@ceppi.net>
2019-10-27 21:41:22 -0400

    Project scaffolding

---
+++ README.md
@@ -0,0 +1,3 @@
+# VIO Bot
+
+A slack bot to help you automate and organize everyday tasks
---
+++ viobot/__init__.py
@@ -0,0 +1,8 @@
+
+__title__ = 'viobot'
+__version__ = '0.0.1'
+__description__ = 'Slack Bot'
+__author__ = 'Marco Ceppi'
+__author_email__ = 'marco@ceppi.net'
+__url__ = 'https://github.com/marcoceppi/vio-bot'
+__license__ = 'GNU General Public License v3.0'
